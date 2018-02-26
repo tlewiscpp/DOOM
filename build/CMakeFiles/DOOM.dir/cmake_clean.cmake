3310bdddaf9edae0ba80605ec6bcaa78f99ae87d
Tyler Lewis <tlewiscpp@gmail.com>
2018-02-26 18:02:04 -0500

    Auto-Commit - Z170A-Titanium-PC

--- build/CMakeFiles/DOOM.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "DOOM.pdb"
-  "DOOM"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DOOM.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/sndserv/CMakeFiles/sndserv.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsndserv.pdb"
-  "libsndserv.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sndserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
